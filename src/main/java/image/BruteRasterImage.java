443b02ec112ce34a9975650a84214d2b519b6fbd
KIB00631 <kevin.ibares@gmail.com>
2018-11-28 11:38:34 +0100

    Création des deux constructeurs de la classe BruteRasterImage et des méthodes de l'interface Image.

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -1,4 +1,51 @@
 package image;
 
-public class BruteRasterImage {
+import javafx.scene.paint.Color;
+import java.util.Arrays;
+import static java.util.Arrays.*;
+import static util.Matrices.*;
+
+
+public class BruteRasterImage implements Image {
+
+    int width;
+    int height;
+    Color [][] colors;
+
+    public BruteRasterImage (Color color, int width, int height){
+        this.width = width;
+        this.height = height;
+        colors = new Color [this.width][this.height];
+        for (Color[] col: colors) {
+            Arrays.fill(col, color);
+        }
+    }
+
+    public BruteRasterImage (Color[][] colors){
+
+        requiresNonNull(colors);
+        requiresNonZeroDimensions(colors);
+        requiresRectangularMatrix(colors);
+        this.colors = colors;
+        this.width = getRowCount(colors);
+        this.height = getColumnCount(colors);
+
+    }
+
+
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return colors[x][y];
+    }
+
+    @Override
+    public int getWidth() {
+        return this.width;
+    }
+
+    @Override
+    public int getHeight() {
+        return this.height;
+    }
 }
