4486037cec6d1d02fb505a91b55232d01749e305
KIB00631 <kevin.ibares@gmail.com>
2018-12-05 11:43:31 +0100

    Création de la classe Rectangle.

---
+++ src/main/java/image/Rectangle.java
@@ -0,0 +1,30 @@
+package image;
+
+import javafx.scene.paint.Color;
+
+public class Rectangle implements Shape {
+    int x;
+    int y;
+    int width;
+    int height;
+    Color color;
+
+    public Rectangle(int x, int y, int width, int height, Color color){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+
+    @Override
+    public boolean contains(Point point) {
+        return ((this.x <= point.x)&& (point.x <= this.width) && (this.y <= point.y)&& (point.y <= this.height));
+    }
+
+    @Override
+    public Color getColor() {
+        return this.color;
+    }
+}
